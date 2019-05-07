72576f876a5fd327e737c6c67d999b8c53f0cf54
Macivor <macivoce@richva.ci.richmond.va.us>
2019-05-07 13:50:36 -0400

    wire up message on the ui

--- wwwroot/Helper.js
+++ wwwroot/Helper.js
@@ -1,6 +1,7 @@
 
 window.CaseForm = window.CaseForm || {};
 window.BingMap = window.BingMap || {};
+window.Register = window.Register || {};
 window.CaseForm.ClearList = (elementId) => {
     //document.getElementById(elementId).innerHTML = "";
     alert(elementId);
@@ -11,4 +12,10 @@ window.BingMap.OpenModalOnSearch = () => {
 };
 window.BingMap.HideModal = () => {
     $("#addressModal").modal('hide');
+};
+window.Register.OpenError = () => {
+    $("#divError").show();
+};
+window.Register.HideError = () => {
+    $("#divError").hide();
 }
